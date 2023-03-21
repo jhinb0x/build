439bbb2b81c352e3e2e05a0b9523fa3bec4d2568
JOSE MAURICIO SILVA CRUZ <00001111450596sp@aluno.educacao.sp.gov.br>
2023-03-21 18:49:18 -0300

    meu primeiro

---
+++ build/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export default function App(){
+  return(
+    <div>
+      <Cabecalho/>
+      <Texto/>
+      <Cabecalho/>
+    </div>
+  )
+}
+
+export function Cabecalho(){
+  return(
+    <div
+    style={{
+    backgroundColor:'black', 
+    height:'20vh'
+    }}
+    >
+      
+    </div>
+  )
+}
+
+export function Texto(){
+  return(
+    <div
+    style={{
+      paddingBottom:'20vh',
+      paddingTop:'20vh'
+    }}
+    >
+      <h1>YASUO E UMA MERDAAAAAAAAAAAAAA</h1>
+    </div>
+  )
+}
